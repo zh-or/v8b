bd3ae0c615c3423f19226a99e93e80c3d184421c
O_R <982465979@qq.com>
2019-01-23 10:01:43 +0800

    create project

---
+++ src/v8base/main/V8BaseTest.java
@@ -0,0 +1,8 @@
+package v8base.main;
+
+public class V8BaseTest {
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/v8base/v8/V8Base.java
@@ -0,0 +1,5 @@
+package v8base.v8;
+
+public class V8Base {
+
+}
